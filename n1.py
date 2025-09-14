23f823b103ed0e98cc56e7808a340413bd5ab155
SomeThingsNeverChange <94125232+SomeThingsNeverChange@users.noreply.github.com>
2025-09-14 13:19:18 +0300

    first commit

---
+++ n1.py
@@ -0,0 +1,9 @@
+
+
+number = input("Input your numbeer: ")
+print(number)
+
+if int(number) > 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
---
+++ n2.py
@@ -0,0 +1,9 @@
+
+
+string = input("Type any string: ")
+
+print("Your string: " + string)
+
+wight = len(string)
+
+print("Number of wight: "+ str(wight))
